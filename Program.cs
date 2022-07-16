29ed8fef7ba496ccfb8509cc9091b3de01917c76
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-16 12:14:55 -0500

    two ways of creating objects in C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
@@ -6,7 +7,21 @@ namespace CatWorx.BadgeMaker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> myScoreBoard = new Dictionary<string, int>();
+
+            myScoreBoard.Add("firstInning", 10);
+            myScoreBoard.Add("secondInning", 20);
+            myScoreBoard.Add("thirdInning", 30);
+            myScoreBoard.Add("fourthInning", 40);
+            myScoreBoard.Add("fifthInning", 50);
+
+            // Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+            // { "firstInning", 10 },
+            // { "secondInning", 20},
+            // { "thirdInning", 30},
+            // { "fourthInning", 40},
+            // { "fifthInning", 50}
+};
         }
     }
 }
