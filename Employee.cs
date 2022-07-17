6024db24a6a05b25f1ae5fe128093dd2c648feef
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-17 07:57:17 -0500

    able to print employees

--- Employee.cs
+++ Employee.cs
@@ -1,18 +1,27 @@
-namespace CatWorx.BadgeMaker
-{
-class Employee
-{
-  public string FirstName; 
-  public string LastName;
-  public Employee(string firstName, string lastName, int id, string photoUrl) {
-      FirstName = firstName;
-      LastName = lastName;
-      Id = id;
-      PhotoUrl = photoUrl;
-  }
+namespace CatWorx.BadgeMaker {
+  class Employee
+  {
+    private string FirstName;
+    private string LastName;
+    private int Id;
+    private string PhotoUrl;
+    public Employee(string firstName, string lastName, int id, string photoUrl) {
+        FirstName = firstName;
+        LastName = lastName;
+        Id = id;
+        PhotoUrl = photoUrl;
+    }
+    public string GetName() {
+        return  FirstName + " " + LastName;
+    }
+  
+    public int GetId() {
+        return Id;
+    }
 
-  public string GetName() {
-    return  FirstName + " " + LastName;
+    public string GetPhotoUrl() {
+        return PhotoUrl;
+
+    }
   }
-}
 }
