501adee7b6f4531281f933d270e4947bef534e71
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-07-17 21:28:43 -0500

    added drawing class and related. Added GetCompanyName method to employee

--- Employee.cs
+++ Employee.cs
@@ -23,5 +23,8 @@ namespace CatWorx.BadgeMaker {
         return PhotoUrl;
 
     }
+    public string GetCompanyName() {
+        return "Cat Worx";
+    }
   }
 }
--- Util.cs
+++ Util.cs
@@ -65,13 +65,35 @@ namespace CatWorx.BadgeMaker
       int EMPLOYEE_ID_WIDTH = BADGE_WIDTH;
       int EMPLOYEE_ID_HEIGHT = 100;
 
-      // instance of WebClient is disposed after code in the block has run
-    using(WebClient client = new WebClient())
-    {
-      for (int i = 0; i < employees.Count; i++)
-      {
+      // Graphics objects
+      StringFormat format = new StringFormat();
+      format.Alignment = StringAlignment.Center;
+      int FONT_SIZE = 32;
+      Font font = new Font("Arial", FONT_SIZE);
+      Font monoFont = new Font("Courier New", FONT_SIZE);
+      SolidBrush brush = new SolidBrush(Color.Black);
 
+      DrawString(String, Font, Brush, RectangleF, StringFormat);
+
+      // instance of WebClient is disposed after code in the block has run
+      using(WebClient client = new WebClient())
+      {
+        for (int i = 0; i < employees.Count; i++)
+        {
+          Stream employeeStream = client.OpenRead(employees[i].GetPhotoUrl());
+
+          Image photo = Image.FromStream(client.OpenRead(employees[i].GetPhotoUrl()));
+          Image background = Image.FromFile("badge.png");
+          Image badge = new Bitmap(BADGE_WIDTH, BADGE_HEIGHT);
+          Graphics graphic = Graphics.FromImage(badge);
+          graphic.DrawImage(background, new Rectangle(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+          graphic.DrawImage(photo, new Rectangle(PHOTO_START_X, PHOTO_START_Y, PHOTO_WIDTH, PHOTO_HEIGHT));
+          badge.Save("data/employeeBadge.png");
+
+          
+
+        }
       }
     }
-    }
+  }
 }
